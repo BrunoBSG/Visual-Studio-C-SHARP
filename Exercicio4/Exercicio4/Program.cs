2d83f57b857546255892a47c0c81f7c5cb083624
BrunoBSG <bsimoesgoulart@gmail.com>
2026-07-19 14:12:10 -0300

    Delete Exercicio4 directory

--- Exercicio4/Exercicio4/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿int idade = 0;
-Console.WriteLine("Qual sua idade?");
-idade = int.Parse(Console.ReadLine());
-idade = 365*idade;
-Console.WriteLine($"Você tem {idade} dias de vida");
-Console.ReadKey();
